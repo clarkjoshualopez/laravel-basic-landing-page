26c7b3c634c76a78c3632951d8939736ff12eeaa
clarkjoshualopez <clarkjoshua.lopez@gmail.com>
2024-03-18 21:52:45 +0800

    update services item

--- resources/views/components/services-section/item.blade.php
+++ resources/views/components/services-section/item.blade.php
@@ -1,9 +1,9 @@
 @props(['title' => '', 'description' => ''])
 
-<div class="text-center flex flex-col items-center">
+<div class="text-center flex flex-col items-center py-6 px-4 group/item hover:shadow-xl">
     <div class="w-fit rounded bg-secondary p-4 md:p-5 mb-4 flex justify-center">
         {{ $icon }}
     </div>
-    <h3 class="text-xl md:text-2xl text-gray-900 tracking-tight mb-4 font-bold">{{ $title }}</h3>
+    <h3 class="text-xl md:text-2xl text-gray-900 tracking-tight mb-4 font-bold group-hover/item:text-secondary">{{ $title }}</h3>
     <p class="text-sm md:text-base leading-normal text-gray-500">{{ $description }}</p>
 </div>
